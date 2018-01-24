61e07da4826de619327dfe384532b2d38f1723cb
Claes Persson <c@cp.se>
2018-01-24 02:05:05 +0100

    markdown parsing, summary field

--- henry.go
+++ henry.go
@@ -10,6 +10,8 @@ import (
 	"time"
 
 	"github.com/BurntSushi/toml"
+	"github.com/microcosm-cc/bluemonday"
+	blackfriday "gopkg.in/russross/blackfriday.v2"
 )
 
 type HenryFile struct {
@@ -18,15 +20,17 @@ type HenryFile struct {
 	SubPath     string
 	Type        HenryFileType
 	Data        []byte
+	Body        string
 	HasMetadata bool
 	Metadata    *HenryFileMetadata
 	Date        time.Time
 }
 
 type HenryFileMetadata struct {
-	Title string `toml:"title"`
-	Date  string `toml:"date"`
-	Draft bool   `toml:"draft"`
+	Title   string    `toml:"title"`
+	Date    time.Time `toml:"date"`
+	Draft   bool      `toml:"draft"`
+	Summary string    `toml:"summary"`
 }
 
 type HenryDocument struct {
@@ -36,6 +40,8 @@ type HenryDocument struct {
 	ContentParagraphs []string
 	Date              time.Time
 	Draft             bool
+	Summary           string
+	SummaryRaw        string
 }
 
 type HenryFileType int
@@ -87,10 +93,76 @@ func classifyHenryFile(file *HenryFile, rootPath *string) error {
 	return nil
 }
 
+func createHenryDocument(file *HenryFile) (*HenryDocument, error) {
+	doc := &HenryDocument{}
+
+	u := blackfriday.Run([]byte(file.Body))
+	h := string(bluemonday.UGCPolicy().SanitizeBytes(u))
+	content := strings.Replace(h, "\n\n", "\n", -1)
+	content = strings.Trim(content, "\n")
+
+	doc.Content = content
+	doc.ContentRaw = file.Body
+	doc.ContentParagraphs = make([]string, 0)
+	for _, paragraph := range strings.SplitAfter(doc.Content, "\n") {
+		doc.ContentParagraphs = append(doc.ContentParagraphs, strings.Trim(paragraph, "\n"))
+	}
+
+	if file.Metadata.Title != "" {
+		doc.Title = file.Metadata.Title
+	} else {
+		doc.Title = file.Name
+	}
+
+	if !file.Metadata.Date.IsZero() {
+		doc.Date = file.Metadata.Date
+	} else {
+		doc.Date = file.Date
+	}
+
+	if file.Metadata.Draft {
+		doc.Draft = file.Metadata.Draft
+	} else {
+		doc.Draft = false
+	}
+
+	if file.Metadata.Summary != "" {
+		su := blackfriday.Run([]byte(file.Metadata.Summary))
+		sh := string(bluemonday.UGCPolicy().SanitizeBytes(su))
+		doc.Summary = sh
+		doc.SummaryRaw = file.Metadata.Summary
+	} else {
+		if len(doc.ContentParagraphs) > 0 {
+			doc.Summary = doc.ContentParagraphs[0]
+		}
+	}
+
+	return doc, nil
+}
+
 func createHenryDocuments(files []*HenryFile) ([]*HenryDocument, error) {
 	docs := make([]*HenryDocument, 0)
 
+	for _, file := range files {
+		doc, err := createHenryDocument(file)
+		if err != nil {
+			debug("%s", err.Error())
+			continue
+		}
+
+		docs = append(docs, doc)
+	}
+
 	return docs, nil
+}
+
+func debug(params ...string) {
+	args := make([]interface{}, len(params)-1)
+	for i := 1; i < len(params); i++ {
+		args[i-1] = params[i]
+	}
+
+	fmt.Printf(fmt.Sprintf("%s\n", params[0]), args...)
 }
 
 func findHenryFiles(rootPath string) ([]*HenryFile, error) {
@@ -124,8 +196,13 @@ func main() {
 		panic(err)
 	}
 
-	for _, henryFile := range henryFiles {
-		fmt.Println(henryFile)
+	henryDocs, err := createHenryDocuments(henryFiles)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, henryDoc := range henryDocs {
+		fmt.Println(henryDoc)
 	}
 }
 
@@ -153,6 +230,7 @@ func readHenryFileMetadata(file *HenryFile) error {
 
 	hdr := string(file.Data[0:3])
 	if hdr != "---" {
+		file.Body = string(file.Data)
 		return nil
 	}
 
@@ -164,6 +242,7 @@ func readHenryFileMetadata(file *HenryFile) error {
 
 	file.HasMetadata = true
 	file.Metadata = &metadata
+	file.Body = headerParts[2]
 
 	return nil
 }
